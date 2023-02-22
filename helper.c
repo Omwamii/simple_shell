d0305d0c18bfade1cd2ffcfa1c5d94240acaa09f
Omwamii <ianalpha@protonmail.com>
2023-02-23 00:59:37 +0300

    fixes

--- helper.c
+++ helper.c
@@ -17,41 +17,48 @@ void print_prompt(char *s)
   *
   *Return: pointer to tokenized string
   */
-char *_strtok(char* str, const char* delim)
+char *_strtok(char *str, const char *delim)
 {
-	static char *current_pos;
-	char *token;
+	static char *splitted, *str_end;
+	char *str_start;
+	unsigned int i, bool;
 
 	if (str != NULL)
 	{
-		current_pos = str;
+		if (cmp_chars(str, delim))
+			return (NULL);
+		splitted = str; /*Store first address*/
+		i = strlen(str);
+		str_end = &str[i]; /*Store last address*/
 	}
+	str_start = splitted;
+	if (str_start == str_end) /*Reaching the end*/
+		return (NULL);
 
-	token = current_pos;
-	while (*current_pos != '\0')
+	for (bool = 0; *splitted; splitted++)
 	{
-		if (*current_pos == *delim)
+		/*Breaking loop finding the next token*/
+		if (splitted != str_start)
+			if (*splitted && *(splitted - 1) == '\0')
+				break;
+		/*Replacing delimiter for null char*/
+		for (i = 0; delim[i]; i++)
 		{
-			*current_pos = '\0';
-			current_pos++;
-			if (token != current_pos)
+			if (*splitted == delim[i])
 			{
-				return (token);
-			}
-			else
-			{
-				token = current_pos;
+				*splitted = '\0';
+				if (splitted == str_start)
+					str_start++;
+				break;
 			}
 		}
-		else
-		{
-			current_pos++;
-		}
+		if (bool == 0 && *splitted) /*Str != Delim*/
+			bool = 1;
 	}
-
-	return (token);
+	if (bool == 0) /*Str == Delim*/
+		return (NULL);
+	return (str_start);
 }
-
 
 /* unused function */
 /**
--- tokenize.c
+++ tokenize.c
@@ -49,3 +49,31 @@ char **tokenize(char *buffer, const char *delim)
 	return (args);
 
 }
+
+/**
+ * cmp_chars - compare chars of strings
+ * @str: input string.
+ * @delim: delimiter.
+ *
+ * Return: 1 if are equals, 0 if not.
+ */
+int cmp_chars(char str[], const char *delim)
+{
+	unsigned int i, j, k;
+
+	for (i = 0, k = 0; str[i]; i++)
+	{
+		for (j = 0; delim[j]; j++)
+		{
+			if (str[i] == delim[j])
+			{
+				k++;
+				break;
+			}
+		}
+	}
+	if (i == k)
+		return (1);
+	return (0);
+}
+
